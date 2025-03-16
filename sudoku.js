52e972353d9e83f9fe37f08ecb4b9d88c5ddc564
alexandraplro <caprjournalism@gmail.com>
2025-03-16 18:09:28 -0400

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // The "Close" button is already handled by Bootstrap's data-bs-dismiss="modal",
   // so no additional JavaScript is required for it.
 });
-});
+}});
   
   
   // Timer setup
